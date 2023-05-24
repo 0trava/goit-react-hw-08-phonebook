c315dbeb6016b700a6d0cfb75421c9afe3ec7b1e
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-24 15:58:12 +0300

    add header style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,10 +5,26 @@ import style from './Header.module.css';
 const Header = () => {
   return (
     <div className={style.container}>
-        <a href="" className={style.icon}>
+        <div className={style.header}>
+            <a href="" className={style.logo_box}>
+                <div className={style.icon}/>
+                <p className={style.logo}> <span>C</span>ONTACT <span>B</span>OOK</p>
+            </a>
 
-        </a>
-        <p>ICON</p>
+
+
+            <div className={style.register_form}>
+                <div className={style.user}>
+                    <div className={style.avatar}></div>
+                    <p className={style.username}>Hello, Sveta</p>
+                </div>
+
+                <div className={style.login}>Log <span>In</span> </div>
+                {/* <div className={style.login}>Log <span>Out</span></div> */}
+            </div>
+        </div>
+
+        
     </div>
   )
 }
