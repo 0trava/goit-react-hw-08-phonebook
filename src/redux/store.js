785f01577842d9f3f4b748ddfc37ed654045d6b3
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-25 00:00:56 +0300

    add

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./tasksContacts";
 import { filterReducer } from "./filterContacts";
-import userSlice from './user/userSlice'
+import userSlice from './user/userSlice';
 
 // ВИВІД ІНФОРМАЦІЇ у консоль REDUX
 export const store = configureStore({
