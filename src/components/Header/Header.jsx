c35ba24a54e68668147f5d5197ffe86194217d66
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-24 23:54:31 +0300

    add name user

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { toggleForm, toggleFormType } from '../../redux/user/userSlice';
+import { getIsLoad  } from "../../redux/selectors";
 
 import style from './Header.module.css';
 
 const Header = () => {
     const dispatch = useDispatch();
+    const isLoading = useSelector(getIsLoad);
 
     const openForm = () => {
         dispatch(toggleFormType("signup"));
@@ -23,13 +25,20 @@ const Header = () => {
 
 
             <div className={style.register_form}>
+                
+                { isLoading ? (   
+                <>                 
                 <div className={style.user}>
                     <img  className={style.avatar} src="https://user-life.com/uploads/posts/2020-03/1584366922_1.png" alt="" />
                     <p className={style.username}>Svetlana</p>
                 </div>
+                <div className={style.login}>Log <span>Out</span></div>
+                </>)
+                : (<div className={style.login} onClick={openForm}>Log <span>In</span> </div>)}
 
-                <div className={style.login} onClick={openForm}>Log <span>In</span> </div>
-                {/* <div className={style.login}>Log <span>Out</span></div> */}
+
+                
+
             </div>
         </div>
 
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,5 +2,6 @@ export const getContacts = state => state.contacts.items;
 export const getIsLoading = state => state.contacts.isLoading;
 export const getError = state => state.contacts.error;
 export const getStatusFilter = state => state.filter;
+export const getIsLoad = state => state.user.currentUser;
 export const getFilteredContacts = state => state.contacts.items.filter(contact =>
     contact.name.toLowerCase().includes(state.filter.toLowerCase()));
