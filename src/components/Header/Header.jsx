2e91537831cadb117d18a01e64395331ef38efd8
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-25 21:50:08 +0300

    add home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { toggleForm, toggleFormType } from '../../redux/user/userSlice';
 import { getIsLoad  } from "../../redux/selectors";
 
-import style from './Header.module.css';
+import css from './Header.module.css';
 
 const Header = () => {
     const dispatch = useDispatch();
@@ -16,25 +16,34 @@ const Header = () => {
 
   return (
 
-        <div className={style.header}>
-            <a href="/" className={style.logo_box}>
-                <div className={style.icon}/>
-                <p className={style.logo}> <span>C</span>ONTACT <span>B</span>OOK</p>
-            </a>
+        <div className={css.header}>
+            <div className={css.contacts__navigate}>
+                    <a href="/" className={css.logo_box}>
+                        <div className={css.icon}/>
+                        <p className={css.logo}> <span>H</span>OME</p>
+                    </a>
+                    {isLoading ? (                    <a href="/contacts" className={css.logo_box2}>
+                        <p className={css.logo}> <span>C</span>ONTACTS</p>
+                    </a>) : (<></>) }
+
+            </div>
 
 
 
-            <div className={style.register_form}>
+
+
+            <div className={css.register_form}>
+
                 
                 { isLoading ? (   
                 <>                 
-                <div className={style.user}>
-                    <img  className={style.avatar} src="https://user-life.com/uploads/posts/2020-03/1584366922_1.png" alt="" />
-                    <p className={style.username}>Svetlana</p>
+                <div className={css.user}>
+                    <img  className={css.avatar} src="https://user-life.com/uploads/posts/2020-03/1584366922_1.png" alt="" />
+                    <p className={css.username}>Svetlana</p>
                 </div>
-                <div className={style.login}>Log <span>Out</span></div>
+                <div className={css.login}>Log <span>Out</span></div>
                 </>)
-                : (<div className={style.login} onClick={openForm}>Log <span>In</span> </div>)}
+                : (<div className={css.login} onClick={openForm}>Log <span>In</span> </div>)}
 
 
                 
