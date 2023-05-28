57a95952ee66259a88106b2ce2bb0766e45a529a
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-28 22:10:59 +0300

    add

--- src/components/Routes/PrivateRoute.jsx
+++ src/components/Routes/PrivateRoute.jsx
@@ -1,13 +1,12 @@
 import { Navigate } from 'react-router-dom';
 
 
-
 export const PrivateRoute =  ({ children}) => {
     const userIsLogin = window.localStorage.getItem('checkLogIn');
 
     return (userIsLogin === 'true') ?  children : <Navigate to='/' />;
     
-    };
+};
 
     export default PrivateRoute;
     
