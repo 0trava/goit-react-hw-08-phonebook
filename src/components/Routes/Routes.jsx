d458b7d601eec0ac61639c843b3e4cfe84553ffe
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-28 20:04:58 +0300

    fix

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -11,12 +11,12 @@ import PrivateRoute from "./PrivateRoute";
 
 const AppRoutes = () => {
   return (
-    <>
-  <Routes>
-        <Route path={ROUTES.CONTACTS} element={<PrivateRoute><ContactsBook/></PrivateRoute>} />
-        <Route path={ROUTES.HOME} index element={<Home />}/>
-        <Route path="*" element={<Navigate to="/" />} />
-  </Routes>
+  <>
+    <Routes>
+          <Route path={ROUTES.HOME} index element={<Home />} />
+          <Route path={ROUTES.CONTACTS} element={<PrivateRoute><ContactsBook/></PrivateRoute>} />
+          <Route path="*" element={<Navigate to={ROUTES.HOME} />} />
+    </Routes>
   </>)
 };
 
